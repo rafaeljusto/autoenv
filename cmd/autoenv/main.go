0d21d236f6baa7000ec4a11700ec129d02b9853d
Rafael Dantas Justo <cadastros@rafael.net.br>
2023-08-09 22:33:20 +0200

    Enhancement: Autoenv is here!
    Welcome to the future.

--- cmd/autoenv/main.go
+++ cmd/autoenv/main.go
@@ -7,7 +7,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<html><body><h1>Hello Teamwork!</h1></body></html>")
+	fmt.Fprintf(w, "<html><body><h1>Autoenv is here!</h1></body></html>")
 }
 
 func main() {
